931ae98f6f0d951d01d7f22f8f3125f822eb0d8c
Raja sekhar <16195105+rsirs@users.noreply.github.com>
2023-02-23 03:10:39 +0000

    fix: html

--- color-wheel.js
+++ color-wheel.js
@@ -1,77 +1,120 @@
-export class ColorWheelPicker {
-    constructor(container, width = 300, height = 300) {
-        this.width = width;
-        this.height = height;
-        this.container = container;
-        this.canvas = document.createElement('canvas');
-        this.container.appendChild(this.canvas);
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
-        this.context = this.canvas.getContext('2d');
-        this.selectedColor = new Color('#ff0000');
-        this.drawWheel();
-        this.canvas.addEventListener('click', event => {
-            const x = event.offsetX;
-            const y = event.offsetY;
-            const imageData = this.context.getImageData(x, y, 1, 1);
-            const color = new Color(imageData.data[0], imageData.data[1], imageData.data[2]);
-            this.selectedColor = color;
-            this.drawWheel();
-            this.onSelect(color);
-        });
+class ColorWheelPicker {
+    constructor(element) {
+      this.element = element;
+      this.canvas = document.createElement('canvas');
+      this.canvas.width = this.element.offsetWidth;
+      this.canvas.height = this.element.offsetHeight;
+      this.ctx = this.canvas.getContext('2d');
+  
+      this.element.appendChild(this.canvas);
+  
+      this.element.addEventListener('mousedown', this.handleMouseDown.bind(this));
+      this.element.addEventListener('touchstart', this.handleTouchStart.bind(this));
+      document.addEventListener('mouseup', this.handleMouseUp.bind(this));
+      document.addEventListener('touchend', this.handleTouchEnd.bind(this));
+  
+      this.color = new Color();
+      this.hue = 0;
+  
+      this.draw();
     }
-
-    drawWheel() {
-        this.context.clearRect(0, 0, this.width, this.height);
-        const centerX = this.width / 2;
-        const centerY = this.height / 2;
-        const radius = this.width / 2;
-        const imageData = this.context.getImageData(0, 0, this.width, this.height);
-        for (let x = 0; x < this.width; x++) {
-            for (let y = 0; y < this.height; y++) {
-                const dx = x - centerX;
-                const dy = y - centerY;
-                const distance = Math.sqrt(dx * dx + dy * dy);
-                if (distance < radius) {
-                    const angle = Math.atan2(dy, dx);
-                    let hue = angle / (2 * Math.PI);
-                    if (hue < 0) {
-                        hue += 1;
-                    }
-                    const saturation = distance / radius;
-                    const lightness = 0.5;
-                    const color = new Color().setHSL(hue, saturation, lightness);
-                    const index = (y * this.width + x) * 4;
-                    imageData.data[index] = color.red;
-                    imageData.data[index + 1] = color.green;
-                    imageData.data[index + 2] = color.blue;
-                    imageData.data[index + 3] = 255;
-                }
-            }
-        }
-        this.context.putImageData(imageData, 0, 0);
-        this.drawSelectedColor();
-    }
-
-    drawSelectedColor() {
-        const centerX = this.width / 2;
-        const centerY = this.height / 2;
-        const radius = (this.width / 2) * this.selectedColor.getSaturation();
-        const angle = this.selectedColor.getHue() * 2 * Math.PI;
-        const x = centerX + radius * Math.cos(angle);
-        const y = centerY + radius * Math.sin(angle);
-        this.context.strokeStyle = '#000000';
-        this.context.beginPath();
-        this.context.arc(x, y, 10, 0, 2 * Math.PI);
-        this.context.stroke();
-    }
-
+  
     setColor(color) {
-        this.selectedColor = color;
-        this.drawWheel();
+      this.color = color;
+      const hsl = color.toHsl();
+      this.hue = hsl.h;
+      this.draw();
+      if (typeof this.onColorChange === 'function') {
+        this.onColorChange(color);
+      }
     }
-
-    onSelect(callback) {
-        this.onSelectCallback = callback;
+  
+    draw() {
+      const centerX = this.canvas.width / 2;
+      const centerY = this.canvas.height / 2;
+      const radius = Math.min(centerX, centerY);
+  
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  
+      // Draw color wheel
+      for (let angle = 0; angle < 360; angle++) {
+        const startAngle = (angle - 2) * Math.PI / 180;
+        const endAngle = angle * Math.PI / 180;
+        this.ctx.beginPath();
+        this.ctx.moveTo(centerX, centerY);
+        this.ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+        this.ctx.closePath();
+  
+        const gradient = this.ctx.createRadialGradient(centerX, centerY, 0, centerX, centerY, radius);
+        gradient.addColorStop(0, `hsl(${angle}, 100%, 50%)`);
+        gradient.addColorStop(1, `hsl(${angle}, 100%, 50%)`);
+        this.ctx.fillStyle = gradient;
+        this.ctx.fill();
+      }
+  
+      // Draw hue marker
+      const angle = this.hue;
+      const markerX = centerX + radius * Math.cos(angle * Math.PI / 180);
+      const markerY = centerY + radius * Math.sin(angle * Math.PI / 180);
+  
+      this.ctx.beginPath();
+      this.ctx.arc(markerX, markerY, radius / 10, 0, 2 * Math.PI);
+      this.ctx.strokeStyle = 'white';
+      this.ctx.lineWidth = 3;
+      this.ctx.stroke();
+  
+      this.ctx.beginPath();
+      this.ctx.arc(markerX, markerY, radius / 10, 0, 2 * Math.PI);
+      this.ctx.strokeStyle = 'black';
+      this.ctx.lineWidth = 1;
+      this.ctx.stroke();
     }
-}
+  
+    handleMouseDown(event) {
+      this.handleColorChange(event);
+      document.addEventListener('mousemove', this.handleMouseMove);
+    }
+  
+    handleMouseMove(event) {
+      this.handleColorChange(event);
+    }
+  
+    handleMouseUp() {
+      document.removeEventListener('mousemove', this.handleMouseMove);
+    }
+  
+    handleTouchStart(event) {
+      this.handleColorChange(event.touches[0]);
+      document.addEventListener('touchmove', this.handleTouchMove.bind(this));
+    }
+  
+    handleTouchMove(event) {
+      event.preventDefault();
+      this.handleColorChange(event.touches[0]);
+    }
+  
+    handleTouchEnd() {
+      document.removeEventListener('touchmove', this.handleTouchMove.bind(this));
+    }
+  
+    handleColorChange(event) {
+      const rect = this.canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event
+      const centerX = this.canvas.width / 2;
+      const centerY = this.canvas.height / 2;
+      const radius = Math.min(centerX, centerY);
+  
+      const dx = x - centerX;
+      const dy = y - centerY;
+      const angle = Math.atan2(dy, dx);
+      this.hue = (angle * 180 / Math.PI + 360) % 360;
+  
+      this.color.setHue(this.hue);
+      this.draw();
+      if (typeof this.onColorChange === 'function') {
+        this.onColorChange(this.color);
+      }
+    }
+  }
+    
