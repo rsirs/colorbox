b3e50170d17de5541d83fe8a0be3bac76ed63cfd
Raja sekhar <16195105+rsirs@users.noreply.github.com>
2023-02-23 13:18:58 +0000

    fix: markup

--- index.js
+++ index.js
@@ -1,2 +1,106 @@
-export * from './color';
-export * from './color-wheel';
+function addThemeListner() {
+
+    const baseColor = new Color(document.getElementById("color-input").value || '0091ea');
+
+    // Calculate the relative luminance of the base color
+    const luminance = 0.2126 * baseColor.r + 0.7152 * baseColor.g + 0.0722 * baseColor.b;
+
+    // Define the light and dark colors based on the relative luminance
+    const lightColor = new Color().setLuminance(luminance + 0.5);
+    const darkColor = new Color().setLuminance(luminance - 0.3);
+
+
+    const getAutoColor = (color) => {
+        const luminance = color.getLuminance();
+        return luminance > 0.5 ? darkColor : lightColor;
+    };
+
+
+    const lightTheme = {
+        "--background-color": lightColor.toHexString(),
+        "--text-color": baseColor.getInverted().toHexString(),
+        "--input-background-color": baseColor.lighten(20).toHexString(),
+        "--input-border-color": baseColor.darken(20).toHexString(),
+        "--input-box-shadow-color": baseColor.alpha(0.1).toRgbaString(),
+        "--color-wheel-border-color": baseColor.lighten(30).toHexString(),
+        "--color-picker-selector-color": "#fff",
+        "--color-picker-selector-box-shadow-color": baseColor.alpha(0.2).toRgbaString(),
+    };
+
+    const darkTheme = {
+        "--background-color": darkColor.toHexString(),
+        "--text-color": baseColor.getInverted().toHexString(),
+        "--input-background-color": baseColor.darken(20).toHexString(),
+        "--input-border-color": baseColor.lighten(20).toHexString(),
+        "--input-box-shadow-color": baseColor.alpha(0.1).toRgbaString(),
+        "--color-wheel-border-color": baseColor.darken(30).toHexString(),
+        "--color-picker-selector-color": "#333",
+        "--color-picker-selector-box-shadow-color": baseColor.alpha(0.2).toRgbaString(),
+    };
+
+    const autoTheme = {
+        "--background-color": getAutoColor(baseColor).toHexString(),
+        "--text-color": baseColor.getInverted().toHexString(),
+        "--input-background-color": baseColor.lighten(5).toHexString(),
+        "--input-border-color": baseColor.darken(10).toHexString(),
+        "--input-box-shadow-color": baseColor.alpha(0.1).toRgbaString(),
+        "--color-wheel-border-color": baseColor.lighten(20).toHexString(),
+        "--color-picker-selector-color": baseColor.getInverted().toHexString(),
+        "--color-picker-selector-box-shadow-color": baseColor.alpha(0.2).toRgbaString(),
+    };
+
+    const toggleSwitch = document.querySelectorAll(".theme-switch input[type='radio']");
+    const currentTheme = localStorage.getItem("theme");
+
+    if (currentTheme) {
+        document.documentElement.setAttribute("data-theme", currentTheme);
+        if (currentTheme === "dark") {
+            applyTheme(darkTheme);
+            toggleSwitch[0].checked = true;
+        } else if (currentTheme === "auto") {
+            applyAutoTheme();
+            toggleSwitch[1].checked = true;
+        } else {
+            applyTheme(lightTheme);
+            toggleSwitch[2].checked = true;
+        }
+    }
+
+    function applyTheme(theme) {
+        Object.keys(theme).forEach((key) => {
+            document.documentElement.style.setProperty(key, theme[key]);
+        });
+    }
+
+    function applyAutoTheme() {
+        if (baseColor.isDark()) {
+            applyTheme(darkTheme);
+            toggleSwitch[0].checked = true;
+            localStorage.setItem("theme", "dark");
+        } else {
+            applyTheme(autoTheme);
+            toggleSwitch[1].checked = true;
+            localStorage.setItem("theme", "auto");
+        }
+    }
+
+    toggleSwitch.forEach((radio) => {
+        radio.addEventListener("change", (e) => {
+            if (e.target.value === "dark") {
+                applyTheme(darkTheme);
+                localStorage.setItem("theme", "dark");
+            } else if (e.target.value === "auto") {
+                applyAutoTheme();
+                localStorage.setItem("theme", "auto");
+            } else {
+                applyTheme(lightTheme);
+                localStorage.setItem("theme", "light");
+            }
+        });
+    });
+
+}
+
+(function setup() {
+    addThemeListner();
+})()
